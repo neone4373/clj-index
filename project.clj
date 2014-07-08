01c510dc53fd497da03d332bd750c8bbaeca0d6e
Neo Ellison <neo@signafire.com>
2014-07-08 19:26:25 -0400

    require java 1.6

--- project.clj
+++ project.clj
@@ -1,10 +1,15 @@
 (defproject neone4373/clj-index "0.0.3"
   :description "String indexing and matching algorithms"
   :dependencies [[org.clojure/clojure "1.4.0"]]
+  :url "https://github.com/neone4373/clj-index"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}   
   :plugins [[lein-swank "1.4.4"]]
   :java-source-paths ["src/main/java"]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-
+  ;:deploy-repositories [["releases" {:sign-releases false}]]
   :compile-path "classes"
+  :signing {:gpg-key "B43988E5"}
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
 )
